b77aa29dc001929c4a62f6d3b886621fa1bf828c
typicalchazz <chase.ryan.dalton@gmail.com>
2020-12-20 14:04:46 -0600

    ex16 improve

---
+++ hw16.py
@@ -0,0 +1,12 @@
+print("What is the name of the file?")
+
+old_file = open(input(),'w')
+
+print(old_file)
+
+print("What is the name of the file u want 2 create??")
+
+new_file = open(input(),'x')
+
+print(new_file)
+
