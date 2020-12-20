ea39cba877ed473ac720437b558c01c67afb8cdb
typicalchazz <chase.ryan.dalton@gmail.com>
2020-12-20 11:58:20 -0600

    Lots of ex

---
+++ ex5.py
@@ -0,0 +1,9 @@
+my_name = 'Chase R. Dalton'
+my_age = 35
+my_height = 68
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Blond'
+
+print(f"Let's talk about {my_name}")
+print(f"He's {my_height} tall")
---
+++ ex9.py
@@ -0,0 +1,11 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug\nSep\nOct\nNov\nDec"
+
+print ("Here are the days: ", days)
+print ("Here are the months: ", months)
+
+print ("""
+There's something going on here.
+With the 3 double quotes.
+We can type all we want
+all the lines yeet""")
